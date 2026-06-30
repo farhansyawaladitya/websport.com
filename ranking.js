044932cf88be9e878279486cc32a34e21abeec17
farhansyawaladitya <farhangt580@gmail.com>
2026-06-30 20:52:20 +0700

    ranking

---
+++ ranking.js
@@ -0,0 +1,23 @@
+// data.js
+const mobilData = [
+    { kode: "A01", nama: "Mercedes-AMG GT", harga: 81, kecepatan: 94, hp: 86, efisiensi: 71, desain: 86, nilaiOreste: 418 },
+    { kode: "A02", nama: "Audi R8", harga: 82, kecepatan: 93, hp: 87, efisiensi: 72, desain: 87, nilaiOreste: 421 },
+    { kode: "A03", nama: "Toyota Supra", harga: 83, kecepatan: 92, hp: 88, efisiensi: 73, desain: 88, nilaiOreste: 424 },
+    { kode: "A04", nama: "Jaguar F-Type", harga: 84, kecepatan: 91, hp: 89, efisiensi: 74, desain: 89, nilaiOreste: 427 },
+    { kode: "A05", nama: "Lexus LC500", harga: 85, kecepatan: 90, hp: 90, efisiensi: 75, desain: 90, nilaiOreste: 430 },
+    { kode: "A06", nama: "Maserati MC20", harga: 86, kecepatan: 89, hp: 91, efisiensi: 76, desain: 91, nilaiOreste: 433 },
+    { kode: "A07", nama: "Lotus Emira", harga: 87, kecepatan: 88, hp: 92, efisiensi: 77, desain: 92, nilaiOreste: 436 },
+    { kode: "A08", nama: "Honda NSX", harga: 88, kecepatan: 95, hp: 93, efisiensi: 78, desain: 85, nilaiOreste: 439 },
+    { kode: "A09", nama: "Bugatti Chiron", harga: 89, kecepatan: 94, hp: 94, efisiensi: 79, desain: 86, nilaiOreste: 442 },
+    { kode: "A10", nama: "Ferrari 488 GTB", harga: 80, kecepatan: 93, hp: 85, efisiensi: 70, desain: 87, nilaiOreste: 415 },
+    { kode: "A11", nama: "Chevrolet Corvette C8", harga: 81, kecepatan: 92, hp: 86, efisiensi: 71, desain: 88, nilaiOreste: 418 },
+    { kode: "A12", nama: "Aston Martin Vantage", harga: 82, kecepatan: 91, hp: 87, efisiensi: 72, desain: 89, nilaiOreste: 421 },
+    { kode: "A13", nama: "Alpine A110", harga: 83, kecepatan: 90, hp: 88, efisiensi: 73, desain: 90, nilaiOreste: 424 },
+    { kode: "A14", nama: "BMW M4 Competition", harga: 84, kecepatan: 89, hp: 89, efisiensi: 74, desain: 91, nilaiOreste: 427 },
+    { kode: "A15", nama: "Ford Mustang GT", harga: 85, kecepatan: 88, hp: 90, efisiensi: 75, desain: 92, nilaiOreste: 430 },
+    { kode: "A16", nama: "Dodge Challenger SRT Hellcat", harga: 86, kecepatan: 95, hp: 91, efisiensi: 76, desain: 85, nilaiOreste: 433 },
+    { kode: "A17", nama: "Lamborghini Huracán EVO", harga: 87, kecepatan: 94, hp: 92, efisiensi: 77, desain: 86, nilaiOreste: 436 },
+    { kode: "A18", nama: "McLaren 720S", harga: 88, kecepatan: 93, hp: 93, efisiensi: 78, desain: 87, nilaiOreste: 439 },
+    { kode: "A19", nama: "Porsche 911 Turbo S", harga: 89, kecepatan: 92, hp: 94, efisiensi: 79, desain: 88, nilaiOreste: 442 },
+    { kode: "A20", nama: "Nissan GT-R R35", harga: 80, kecepatan: 91, hp: 85, efisiensi: 70, desain: 89, nilaiOreste: 415 }
+];
